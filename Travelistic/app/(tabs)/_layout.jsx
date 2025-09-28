bffc75300eaa5871912c706f30e3278ddb189d1f
perlancc <pcaceres2005@gmail.com>
2025-09-28 10:27:51 -0400

    added bottom tab

--- Travelistic/app/(tabs)/_layout.jsx
+++ Travelistic/app/(tabs)/_layout.jsx
@@ -1,1 +1,34 @@
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#6366F1',
+        headerShown: false,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <Ionicons name="home" size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="summary"
+        options={{
+          title: 'Summary',
+          tabBarIcon: ({ color }) => <Ionicons name="calendar" size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <Ionicons name="search" size={28} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
